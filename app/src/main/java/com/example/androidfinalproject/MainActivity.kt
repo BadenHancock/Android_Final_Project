d1a5e04138014e704c2eb23bedb2e76518b51d3b
hancockb24 <hancockb24@hsnet.ahsd.org>
2023-04-18 10:15:16 -0400

    learn

--- app/src/main/java/com/example/androidfinalproject/MainActivity.kt
+++ app/src/main/java/com/example/androidfinalproject/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.androidfinalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("Main Activity","testing")
         setContentView(R.layout.activity_main)
     }
 }
