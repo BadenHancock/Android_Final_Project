63c65c1eb7f75acda93dfd41826eff67e1667ddd
hancockb24 <hancockb24@hsnet.ahsd.org>
2023-04-21 10:37:05 -0400

    4/21/23

---
+++ app/src/main/java/com/example/androidfinalproject/FirebaseFragment.kt
@@ -0,0 +1,23 @@
+package com.example.androidfinalproject
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+
+class FirebaseFragment : Fragment() {
+
+    lateinit var dbRef : DatabaseReference
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        dbRef = Firebase.database.reference
+        return inflater.inflate(R.layout.fragment_firebase, container, false)
+    }
+}
