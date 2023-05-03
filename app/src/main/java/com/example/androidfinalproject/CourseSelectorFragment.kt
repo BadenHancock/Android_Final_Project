6db8d405c8e709cb81d91d3637c6d07ea4eb4a7c
hancockb24 <hancockb24@hsnet.ahsd.org>
2023-05-03 10:35:37 -0400

    5/3/23

--- app/src/main/java/com/example/androidfinalproject/CourseSelectorFragment.kt
+++ app/src/main/java/com/example/androidfinalproject/CourseSelectorFragment.kt
@@ -5,14 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.androidfinalproject.databinding.FragmentCourseSelectorBinding
+import com.example.androidfinalproject.databinding.FragmentHomeBinding
 
 class CourseSelectorFragment : Fragment() {
 
+    private var _binding: FragmentCourseSelectorBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentCourseSelectorBinding.inflate(inflater, container, false)
+        val rootView = binding.root
 
-        return inflater.inflate(R.layout.fragment_course_selector, container, false)
+
+        return rootView
     }
 }
